707de28497615d943650284dee33ff4975af733f
Aghori The King <55387030+devallareddyb@users.noreply.github.com>
2024-04-30 18:36:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ apk add docker-compose
 docker volume create portainer_data
 docker volume create myst-data
 
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest --admin-password=Login@123@@@
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 docker run --cap-add NET_ADMIN --net host --name myst -v myst-data:/var/lib/mysterium-node -d mysteriumnetwork/myst service --agreed-terms-and-conditions
 docker run -d --name packetstream --restart=always -e CID=6HID packetstream/psclient:latest
 docker run -d --name grass --restart=always -e GRASS_USER=devipearn@gmail.com -e GRASS_PASS=Login@123@@@ mrcolorrain/grass
